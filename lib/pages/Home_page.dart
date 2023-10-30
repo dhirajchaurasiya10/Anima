17f5f644f2478b6718ce524b5ff723945611fe38
Dhiraj <starkjon224@gmail.com>
2023-10-30 22:49:25 +0545

    final

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -7,12 +7,24 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
   double _buttonradius = 100;
 
   final Tween<double> _backgroundscale = Tween<double>(begin: 0.0, end: 1.0);
 
+  AnimationController? _stariconcontroller;
+
   @override
+  void initState() {
+    super.initState();
+    _stariconcontroller = AnimationController(
+        vsync: this,
+        duration: const Duration(
+          seconds: 4,
+        ));
+    _stariconcontroller!.repeat();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
@@ -21,7 +33,15 @@ class _HomepageState extends State<Homepage> {
           clipBehavior: Clip.none,
           children: [
             _pagebackground(),
-            _circularanimationbtn(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _circularanimationbtn(),
+                _staricon(),
+              ],
+            ),
           ],
         ),
       ),
@@ -71,6 +91,23 @@ class _HomepageState extends State<Homepage> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _staricon() {
+    return AnimatedBuilder(
+      animation: _stariconcontroller!.view,
+      builder: (_buildContext, _child) {
+        return Transform.rotate(
+          angle: _stariconcontroller!.value * 2 * 3.14,
+          child: _child,
+        );
+      },
+      child: Icon(
+        Icons.star,
+        size: 100,
+        color: Colors.white,
       ),
     );
   }
